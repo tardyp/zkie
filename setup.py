eb901708e2e9e303a98531a4f6a5f8b2197e0bbd
Pierre Tardy <tardyp@gmail.com>
2016-11-09 14:55:51 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 setup(
     name="zkie",
-    version="0.1.1",
+    version="0.1.2",
     description="zookeeper commandline, like httpie but for zookeeper",
     long_description=README,
     author="Pierre Tardy",
